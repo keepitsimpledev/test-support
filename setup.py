b3effa38f08f90f76a26dacbdffe57059587dff5
dev <keep.it.simple.dev@gmail.com>
2020-12-27 19:22:02 -0500

    add packages

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(name='test-support',
       version='1.0',
-      url='https://github.com/keepitsimpledev/test-support')
+      url='https://github.com/keepitsimpledev/test-support',
+      packages=find_packages()
+      )
